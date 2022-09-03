d6ffb6ba5449218be21f929d27631185b86c6229
KvetoslavPrikryl <kvetoslav.prikryl1991@gmail.com>
2022-09-03 15:59:08 +0200

    Styling KennelImg.js

--- src/components/KennelImg.js
+++ src/components/KennelImg.js
@@ -3,8 +3,28 @@ import "./KennelImgStyles.css";
 
 const KennelImg = () => {
   return (
-    <div className='img'>
-        
+    <div className='kennel-container'>
+        <div className='img'></div>
+        <div className='kennel-content'>
+            <div className='kennel-dog kennel-text' >
+                <button className='btn'>Psi</button>
+                <ul>
+                    <li>
+                        <h3>Jméno psa</h3>
+                        <p>Text o psovi</p>
+                    </li>
+                </ul>
+            </div>
+            <div className='kennel-bitch-dog kennel-text'>
+                <button className='btn'>Feny</button>
+                <ul>
+                    <li>
+                        <h3>Jméno psa</h3>
+                        <p>Text o psovi</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
   )
 }
