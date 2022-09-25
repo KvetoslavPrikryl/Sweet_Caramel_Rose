0b21b543efcd42a8a78bac94a9b08c6af6df5d59
KvetoslavPrikryl <kvetoslav.prikryl1991@gmail.com>
2022-09-25 20:07:55 +0200

    Creation of dog boxes and buttons in KennelImg.js. Styling KennelImgStyles.css

--- src/components/CuttingImg.js
+++ src/components/CuttingImg.js
@@ -3,7 +3,25 @@ import "./CuttingImgStyles.css"
 
 const CuttingImg = () => {
   return (
-    <div className='cutting-container'>CuttingImg</div>
+    <div className='cutting-container'>
+      <div className='cutting-table'>
+        <h1 className='title-cutting'>Stříhání</h1>
+        <table>
+          <tr>
+            <td>Stříhání:</td>
+            <td>300 Kč</td>
+          </tr>
+          <tr>
+            <td>Koupání:</td>
+            <td>100 Kč</td>
+          </tr>
+          <tr>
+            <td>Stříhání + koupání:</td>
+            <td>350 Kč</td>
+          </tr>
+        </table>
+      </div>
+    </div>
   )
 }
 
--- src/components/KennelImg.js
+++ src/components/KennelImg.js
@@ -1,43 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./KennelImgStyles.css";
+import dog1 from "../img/Dog1.jpg"
 
-const KennelImg = () => {
-  return (
-    <div className='kennel-container'>
-        <div className='img'></div>
-        <div className='kennel-content'>
-            <div className='kennel-dog-parents'>
-                <h2>Rodiče</h2>
-                <div className='dog'>
-                    <h3>Pes</h3>
-                    <p>Popis psa</p>
+
+const dogMan = [
+    {name: "Jmeno 1 psa", img: dog1, text : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Curabitur sagittis hendrerit ante. Integer tempor. Aliquam in lorem sit amet leo accumsan lacinia. "},
+    {name: "Jmeno 2 psa", img: "../img/Dog1.jpg", text : "Text o psovi."},
+    {name: "Jmeno 3 psa", img: "../img/Dog1.jpg", text : "Text o psovi."},
+    {name: "Jmeno 4 psa", img: "../img/Dog1.jpg", text : "Text o psovi."},
+
+
+]
+
+const dogWomen = [
+    {name: "jmeno 1 psa", text: "Text o psovi."},
+    {name: "jmeno 1 psa", text: "Text o psovi."},
+    {name: "jmeno 1 psa", text: "Text o psovi."}
+]
+
+function KennelImg () {
+    const [dogShow, dogSetShow] = useState(false)
+    const [bitchShow, bitchSetShow] = useState(false)
+    return (
+        <div className='kennel-container'>
+            <div className='kennel-dog'>
+                <div className='dog-man'>
+                    <button onClick={()=>dogSetShow(!dogShow)} className='btn btn-kennel'>Psi</button>
+                    {dogShow? <div>
+                        {dogMan.map((dog, index) => <div key={index} className="dog-box1">
+                            <h2>{dog.name}</h2>
+                            <div className='dog-info'>
+                                <img style={{width:150, height:100}} src={dog.img} alt='dog'/>
+                                <p>{dog.text}</p>
+                            </div>
+                        </div>)}
+                    </div>:null}
                 </div>
-                <div className='dog'>
-                    <h3>Fena</h3>
-                    <p>Popis psa</p>
+                <div className='dog-woman'>
+                    <button onClick={()=>bitchSetShow(!bitchShow)} className='btn btn-kennel'>Feny</button>
+                    {bitchShow?<di>
+                        {dogWomen.map((dog, index) => <div key={index} className="dog-box2">
+                        <h2>{dog.name}</h2>
+                        <p>{dog.text}</p>
+                    </div>)}
+                    </di>:null}
                 </div>
-            </div>
-            <h2 className="">Potomci</h2>
-            <div className='kennel-dog kennel-text' >
-                <button className='btn'>Psi</button>
-                <ul>
-                    <li className='dog'>
-                        <h3>Jméno psa</h3>
-                        <p>Text o psovi</p>
-                    </li>
-                </ul>
-            </div>
-            <div className='kennel-bitch-dog kennel-text'>
-                <button className='btn'>Feny</button>
-                <ul>
-                    <li className='dog'>
-                        <h3>Jméno psa</h3>
-                        <p>Text o psovi</p>
-                    </li>
-                </ul>
             </div>
         </div>
-    </div>
   )
 }
 
