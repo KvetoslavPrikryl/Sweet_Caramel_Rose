6c07955b4b82af16febd039ba9a02d1c4f239754
KvetoslavPrikryl <kvetoslav.prikryl1991@gmail.com>
2022-11-21 18:59:17 +0100

    Create CuttingGalery and this  CSS.

---
+++ src/components/CuttingGalery.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./CuttingGraleryStyles.css"
+
+function CuttingGalery () {
+    return(
+        <div className='cuting-galery'>
+            <h2>Galerie</h2>
+        </div>
+    )
+}
+
+export default CuttingGalery
--- src/routes/Cutting.js
+++ src/routes/Cutting.js
@@ -2,12 +2,14 @@ import React from 'react'
 import CuttingImg from '../components/CuttingImg';
 import Footer from '../components/Footer';
 import Navbar from "../components/Navbar";
+import CuttingGalery from "../components/CuttingGalery"
 
 
 const Cutting = () => {
   return (
     <div>
         <CuttingImg />
+        <CuttingGalery />
         <Navbar />
         <Footer />
     </div>
